97f3047be7d559637a66a2f21d7164eae98be5ca
Ryan Dailey <ryandailey100@gmail.com>
2018-10-10 20:01:54 -0400

    Added files

---
+++ ActionButton/Example/ActionButton/ViewController.swift
@@ -0,0 +1,50 @@
+// ViewController.swift
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 ActionButton
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var actionButton: ActionButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let twitterImage = UIImage(named: "twitter_icon.png")!
+        let plusImage = UIImage(named: "googleplus_icon.png")!
+        
+        let twitter = ActionButtonItem(title: "Twitter", image: twitterImage)
+        twitter.action = { item in print("Twitter...") }
+        
+        let google = ActionButtonItem(title: "Google Plus", image: plusImage)
+        google.action = { item in print("Google Plus...") }
+        
+        actionButton = ActionButton(attachedToView: self.view, items: [twitter, google])
+        actionButton.action = { button in button.toggleMenu() }
+        actionButton.setTitle("+", forState: UIControlState())
+        
+        actionButton.backgroundColor = UIColor(red: 238.0/255.0, green: 130.0/255.0, blue: 34.0/255.0, alpha:1.0)
+    }
+}
+
